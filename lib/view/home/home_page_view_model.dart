ad6618e503622cc76632ad6241d49ab6e7615cf0
clean17 <piw940317@gmail.com>
2023-04-04 11:41:53 +0900

    유연한 State프로바이더 및 Dto 생성

--- lib/view/home/home_page_view_model.dart
+++ lib/view/home/home_page_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dto/home_page_response_dto.dart';
 
 // 이 파일의 목적은 홈페이지에서 사용될 모델을 다운, 저장, 관리
 
@@ -30,31 +31,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // StateNotifierProvider 첫번째 인자는 관리하려는 스토어를 넣는다.
 // StateNotifierProvider 두번째 인자는 스토어에서 관리할 상태를 넣는다.
 final homePageViewModel = StateNotifierProvider<HomePageViewModel, HomePageModel>((ref) {
-  return HomePageViewModel(HomePageModel());
+  return HomePageViewModel(HomePageModel()); // required 를 사용하면 매번 코드를 수정해야한다.
 });
-
-// 스토어 데이터
-class HomePageModel {
-  // 여러 데이터를 가지므로 리스트를 가진다.
-  List<Post> posts = [];
-}
-
-// 데이터가 변하기 때문에 불변성을 가진 모델로는 부적함
-// 재사용하지 않고 여기서만 사용한다.
-class User {
-}
-
-class Post {
-}
-
-
-// dart에서는 inner 클래스가 안된다.
-// dto를 파싱한 dart 오브젝트는 재사용 해서는 안될까?
-
 
 // 이름이 비슷해서 헷갈리겠지만
 // HomePageModel 를 관리하는 HomePageViewModel 스토어(창고)
 class HomePageViewModel extends StateNotifier<HomePageModel> {
   HomePageViewModel(super.state);
-  // 스토어를 관리하기 위해서 관리하는 메소드가 필요하다.
+// 스토어를 관리하기 위해서 관리하는 메소드가 필요하다.
 }
+
+// 스토어 데이터
+class HomePageModel {
+  // 여러 데이터를 가지므로 리스트를 가진다.
+  List<HomePagePostDto>? posts; // required 를 사용하면 매번 코드를 수정해야 하므로 ? 사용
+  HomePageModel({this.posts});
+}
+
+// 데이터가 변하기 때문에 불변성을 가진 모델은 부적합
+// 재사용하지 않고 여기서만 사용한다.
+
+
